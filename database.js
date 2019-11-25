57200121b46300789fa1b868e8de62822d262d01
linankim <52687026+linankim@users.noreply.github.com>
2019-11-25 15:06:48 +0700

    connecting database

---
+++ database.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true }, err => {
+  err ? console.log(err) : console.log("Connected to MongoDB.");
+});
+
+module.exports = mongoose;
