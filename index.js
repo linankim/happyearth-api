dbedabfe309ba4c1618f9f182618686fe9706ad1
Annika Heuler-Kottmann <annikaheuler@gmx.de>
2019-11-25 16:05:32 +0700

    connecting the database 2

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+require("dotenv").config();
+require("./database");
+
 app.listen(4000, () => {
   console.log("Ready on Port 4000");
 });
