e18bb1106a900e4613c0dcdcfd5102a5aeb2335a
linankim <52687026+linankim@users.noreply.github.com>
2019-11-26 11:16:01 +0700

    signup controller

--- models/users.js
+++ models/users.js
@@ -1,24 +1,25 @@
-const mongoose = require("../database");
+const mongoose = require('../database')
 
-const Users = mongoose.model("users", {
-  firstName: {
-    type: String,
-    required: [true, "name is required"]
-  },
-  lastName: {
-    type: String,
-    required: [true, "name is required"]
-  },
-  residenceCountry: {
-    type: String,
-    required: [true, "name is required"]
-  },
-  email: {
-    type: String,
-    required: [true, "name is required"]
-  },
-  password: {
-    type: String,
-    required: [true, "name is required"]
-  }
-});
+const Users = mongoose.model('users', {
+	firstName: {
+		type: String,
+		required: [true, 'name is required']
+	},
+	lastName: {
+		type: String,
+		required: [true, 'name is required']
+	},
+	residenceCountry: {
+		type: String,
+		required: [true, 'name is required']
+	},
+	email: {
+		type: String,
+		required: [true, 'name is required']
+	},
+	password: {
+		type: String,
+		required: [true, 'name is required']
+	},
+	avatar: String
+})
