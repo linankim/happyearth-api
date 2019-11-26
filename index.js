5f4a698b5684a1495b61aae5dcafce4a7eeaf5b3
Annika Heuler-Kottmann <annikaheuler@gmx.de>
2019-11-26 10:53:30 +0700

    controller Login

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
-
 require("dotenv").config();
+
+//connect database
 require("./database");
 
-app.listen(4000, () => {
-  console.log("Ready on Port 4000");
+//middleware
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors({ credentials: true }));
+
+//routes
+app.post("/login", require("./controllers/Login"));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Ready on port ${process.env.PORT}`);
 });
+
+//exports
+module.exports = app;
--- models/users.js
+++ models/users.js
@@ -3,22 +3,24 @@ const mongoose = require("../database");
 const Users = mongoose.model("users", {
   firstName: {
     type: String,
-    required: [true, "name is required"]
+    required: [true, "First name is required"]
   },
   lastName: {
     type: String,
-    required: [true, "name is required"]
+    required: [true, "Last name is required"]
   },
   residenceCountry: {
     type: String,
-    required: [true, "name is required"]
+    required: [true, "Residence country is required"]
   },
   email: {
     type: String,
-    required: [true, "name is required"]
+    required: [true, "Email is required"]
   },
   password: {
     type: String,
-    required: [true, "name is required"]
+    required: [true, "Password is required"]
   }
 });
+
+module.exports = Users;
