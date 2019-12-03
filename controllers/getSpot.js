54affb60314250cabef872c2478e92858e3a0c01
Annika Heuler-Kottmann <annikaheuler@gmx.de>
2019-12-03 14:15:49 +0700

    updated populate for getSpot

--- controllers/getSpot.js
+++ controllers/getSpot.js
@@ -3,7 +3,7 @@ const Spots = require('../models/spots')
 module.exports = (req, res) => {
 	console.log('connected')
 	Spots.findById(req.params.id)
-		.populate('users')
+		.populate('users amenities types')
 		.lean()
 		.then(data => {
 			console.log('req.params.id', req.params.id)
