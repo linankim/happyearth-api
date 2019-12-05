dc88a3177481699cb25fe3361d8dc6b2036d4236
Annika Heuler-Kottmann <annikaheuler@gmx.de>
2019-12-05 15:27:47 +0700

    created picture upload for signup controller

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,28 +1,52 @@
-//local host stuff is missing
 const Users = require('../models/users')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const DataUri = require('datauri')
+const path = require('path')
+const cloudinary = require('cloudinary')
 
 module.exports = (req, res) => {
+	console.log('req', req.body)
+	console.log('req.body.email', req.body.email)
 	Users.findOne({ email: req.body.email })
 		.select('password')
 		.then(user => {
 			if (user) {
 				res.send('This email is already in use.')
+				console.log('stop here')
 			} else {
 				req.body.password = bcrypt.hashSync(req.body.password, 10)
-				console.log('req.body', req.body.password)
-				Users.create(req.body)
-					.then(user => {
-						let token = jwt.sign(user.toObject(), process.env.SECRET)
-						//{token} or token, up to you
-						res.send({ token: token })
+				console.log('encrypted password', req.body.password)
+
+				if (req.file) {
+					cloudinary.config({
+						cloud_name: process.env.CLOUDNAME,
+						api_key: process.env.APIKEY,
+						api_secret: process.env.APISECRET
 					})
-					.catch(err => res.send(err))
-					//review this!!!this was caught by tests
-					.catch(err => {
-						res.send(err)
+					const dataUri = new DataUri()
+					let uri = dataUri.format(
+						path.extname(req.file.originalname).toString(),
+						req.file.buffer
+					).content
+					cloudinary.uploader.upload(uri).then(cloudinaryFile => {
+						console.log({ cloudinaryFile })
+						req.body.avatar = cloudinaryFile.url
+
+						Users.create(req.body)
+							.then(user => {
+								let token = jwt.sign(user.toObject(), process.env.SECRET)
+
+								res.send({ token: token })
+							})
+							.catch(err => res.send(err))
 					})
+				} else {
+					console.log('something')
+				}
 			}
+		})
+		.catch(err => {
+			res.send(err)
 		})
 }
