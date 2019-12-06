8e9f2f69fa4318e114d42048384a81aa82684530
Annika Heuler-Kottmann <annikaheuler@gmx.de>
2019-12-06 08:03:32 +0700

    single file upload to patchUser controllers works

--- controllers/patchUser.js
+++ controllers/patchUser.js
@@ -5,9 +5,11 @@ const cloudinary = require('cloudinary')
 
 module.exports = (req, res) => {
 	console.log('req.body', req.body)
-	Users.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true })
+	console.log('req.file', req.file)
+	Users.findById({ _id: req.params.id })
 		.then(data => {
 			if (req.file) {
+				console.log('here')
 				cloudinary.config({
 					cloud_name: process.env.CLOUDNAME,
 					api_key: process.env.APIKEY,
@@ -23,9 +25,20 @@ module.exports = (req, res) => {
 					.then(cloudinaryFile => {
 						console.log({ cloudinaryFile })
 						req.body.avatar = cloudinaryFile.url
-
+						console.log('req.body.avatar', req.body.avatar)
 						console.log(data)
 						res.send({ data })
+						Users.findByIdAndUpdate({ _id: req.params.id }, req.body, {
+							new: true
+						})
+							.then(data => {
+								res.send(data)
+								console.log(data)
+							})
+							.catch(err => {
+								console.log(err)
+								res.send(err)
+							})
 					})
 					.catch(err => res.send(err))
 			} else {
