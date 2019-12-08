8d2cfb15c921e22e89a5769de197eff42cd10eab
Annika Heuler-Kottmann <annikaheuler@gmx.de>
2019-12-08 10:58:51 +0700

    inserted eatin and takeaway into getSpot

--- controllers/getSpot.js
+++ controllers/getSpot.js
@@ -3,7 +3,7 @@ const Spots = require('../models/spots')
 module.exports = (req, res) => {
 	console.log('connected')
 	Spots.findById(req.params.id)
-		.populate('users amenities types')
+		.populate('users eatin takeaway types')
 		.lean()
 		.then(data => {
 			console.log('req.params.id', req.params.id)
