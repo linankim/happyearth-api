92ab7e84e8fea4ccfce4bb15d42c8a85d61163eb
linankim <52687026+linankim@users.noreply.github.com>
2019-12-08 13:18:58 +0700

    CITIESSSSSSS

--- controllers/getCities.js
+++ controllers/getCities.js
@@ -3,11 +3,22 @@ const Spots = require('../models/spots')
 module.exports = (req, res) => {
 	Spots.find({})
 		.select('city')
-		.then(data => {
-			// filter data, only return spots where city === req.query.name
-			// remove duplicates
-			console.log({ data })
-			res.send(data)
+		.then(spots => {
+			let spotsCopy = []
+			spots.forEach(spot => {
+				if (spot.city.indexOf(req.query.name) != -1) {
+					spotsCopy.push(spot)
+				}
+
+				// filter data, only return spots where city === req.query.name
+				// remove duplicates
+			})
+			if (spotsCopy) {
+				spotsOne = []
+				spotsOne.push(spotsCopy[0])
+				console.log({ spotsOne })
+				res.send(spotsOne)
+			}
 		})
 		.catch(err => {
 			console.log(err)
