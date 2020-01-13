44cd2bdd11303d726c9ec5ca02494b761df7e1ec
Annika Heuler-Kottmann <annikaheuler@gmx.de>
2020-01-13 09:02:52 +0100

    delete console.log from deleteUser controller

--- controllers/deleteUser.js
+++ controllers/deleteUser.js
@@ -1,10 +1,8 @@
 const Users = require('../models/users')
 
 module.exports = (req, res) => {
-	console.log('works')
 	Users.findByIdAndDelete({ _id: req.params.id })
 		.then(data => {
-			console.log(data)
 			res.send(data)
 		})
 		.catch(error => {
